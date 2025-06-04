69b63cc93188fdc3534ab99fee48ff898bcfb5f7
Supra-Mk4-1998 <sr7773879@gmail.com>
2025-06-04 22:46:34 +0530

    today's progress

--- Instagram/index.js
+++ Instagram/index.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 
 // app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/JS")));
+app.use(express.static(path.join(__dirname, "public/css")));
 
 
 app.set("views", path.join(__dirname, "/views"));
