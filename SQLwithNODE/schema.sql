69862cd157094f39b1ef7566d25c1aeab833e880
Supra-Mk4-1998 <sr7773879@gmail.com>
2025-06-25 23:26:59 +0530

    schema.sql file

---
+++ SQLwithNODE/schema.sql
@@ -0,0 +1,22 @@
+-- USE aadmi_aurat;
+-- source schema.sql;
+
+CREATE TABLE IF NOT EXISTS user (
+    id VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+-- OR 
+-- IF NOT EXISTS (SELECT * FROM information_schema.tables WHERE table_name = 'user') THEN
+--     CREATE TABLE user (
+--         id VARCHAR(50) PRIMARY KEY,
+--         username VARCHAR(50) UNIQUE,
+--         email VARCHAR(50) UNIQUE NOT NULL,
+--         password VARCHAR(50) NOT NULL
+--     );
+-- END IF;
+
+
+SHOW TABLES;
